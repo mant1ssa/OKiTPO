3a1125af17c3d2a2bf926b2d77ca8ed97edc2c44
mant1ssa <nayrix.8@gmail.com>
2022-03-12 20:31:06 +0500

    now we have working registration sistem

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -19,16 +20,6 @@ const (
 	NEPROPUSTIL        // Непропустил пешехода,      		 код 5
 )
 
-// метод проверки наличия данного пользователя в базе пользователей, принимает номер авто, пароль и размер базы
-/*func isRegistered(num string, psw string) bool {
-	for i := 0; i < a.Size; i++ {
-		if (a.UsersOfSite[i].StateNumAuto == num) && (a.UsersOfSite[i].password == psw) {
-			return true
-		}
-	}
-	return false
-}*/
-
 // Структура Нарушение, необходима для отображения полей таблицы, будем выводить на экран в сайте
 type Violation struct {
 	Num  string `json:"StateNumAuto"` // гос. номер автомобиля нарушителя
@@ -37,26 +28,34 @@ type Violation struct {
 	Fine int    `json:"Fine"`         // Размер штрафа
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
-	/*type input struct { // Определим структуру полученных из окна авторизации данных (ФИО, номер и тд)
-		Name         string
-		Surname      string
-		Otchestvo    string
-		StateNumAuto string
-		Password     string
+func isCorrect(num string) bool { // Функция проверки правильности ввода гос.номера (с 2й по 4й д.б. цифры)
+	if _, err := strconv.Atoi(num[1:4]); err == nil {
+		return true
 	}
-	newUser := input{
-		Name: r.FormValue("user_name"),
-		Surname: r.FormValue("user_surname"),
-		Otchestvo: r.FormValue("user_otchestvo"),
-		StateNumAuto: r.FormValue("user_stateNum"),
-		Password: r.FormValue("user_password"),
-	}*/
-	db, err := sql.Open("mysql", "root:mysql@/tpo")
+	return false
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:mysql@/tpo") // Подключаемся к БД
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	type input struct { // Определим структуру, в ней будут данные из таблицы users (будет проверка на нового пользователя)
+		StateNumAuto string
+		Name         string
+		Surname      string
+		Otchestvo    string
+		Password     string
+	}
+
+	inp, err := db.Query("SELECT * FROM `users`")
+	if err != nil {
+		panic(err)
+	}
+
+	defer inp.Close()
 
 	fmt.Println("Метод:", r.Method) // получаем информацию о методе запроса
 	if r.Method == "GET" {
@@ -73,19 +72,48 @@ func login(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Пароль:", r.Form["user_password"])
 		fmt.Println("Номер авто:", r.Form["user_stateNum"])
 
-		// В эти переменные мы присваиваем значения, введенные пользователем в форме, r.FormValue() - метод, внутрь которой можно запихнуть название формочки для ввода, в registrate.html есть аргументы name="...."
+		// В эти переменные мы получаем введенные пользователем данные, r.FormValue() - метод, внутрь которой можно запихнуть название формочки для ввода, в registrate.html есть аргументы name="...."
 		name := r.FormValue("user_name")
 		surname := r.FormValue("user_surname")
 		otchestvo := r.FormValue("user_otchestvo")
 		stateNum := r.FormValue("user_stateNum")
 		password := r.FormValue("user_password")
-		// Здесь в таблицу users нашей БД вносим эти значения
-		insert, err := db.Query(fmt.Sprintf("INSERT INTO `users` (`StateNumAuto`, `Name`, `Surname`, `Otchestvo`, `password`) VALUES ('%s', '%s', '%s', '%s', '%s')", stateNum, name, surname, otchestvo, password))
-		if err != nil {
-			panic(err)
+
+		if !isCorrect(stateNum) {
+			fmt.Fprintf(w, "Введите правильный номер авто!")
+			http.Redirect(w, r, "/login", 301)
 		}
-		defer insert.Close()
-		http.Redirect(w, r, "/", 301)
+
+		// Проверка того, что в БД уже нет пользователя с данным гос.номером. Он не может повторяться
+		allUsers := []input{}
+		for inp.Next() {
+			p := input{}
+			err := inp.Scan(&p.StateNumAuto, &p.Name, &p.Surname, &p.Otchestvo, &p.Password)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			allUsers = append(allUsers, p)
+		}
+		flag := false
+		for i, _ := range allUsers {
+			if allUsers[i].StateNumAuto == stateNum {
+				flag = true
+			}
+		}
+
+		if !flag {
+			// Здесь в таблицу users нашей БД вносим эти значения
+			insert, err := db.Query(fmt.Sprintf("INSERT INTO `users` (`StateNumAuto`, `Name`, `Surname`, `Otchestvo`, `password`) VALUES ('%s', '%s', '%s', '%s', '%s')", stateNum, name, surname, otchestvo, password))
+			if err != nil {
+				panic(err)
+			}
+			defer insert.Close()
+			http.Redirect(w, r, "/login", 301)
+		} else {
+			fmt.Fprintf(w, "Похоже вы уже зарегестрированы!")
+			http.Redirect(w, r, "/login", 301)
+		}
 	}
 }
 
